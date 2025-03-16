f015cc21925d2762db4b0e35f719f1b09f69d96b
baygut <berkaybaygut@gmail.com>
2025-03-16 13:59:49 +0300

    update: version

--- Package.swift
+++ Package.swift
@@ -1,25 +1,24 @@
-// swift-tools-version:5.3
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "AlonIOS",
-    platforms: [
-        .iOS(.v13)
-    ],
     products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "AlonIOS",
             targets: ["AlonIOS"]),
     ],
-    dependencies: [
-        // No external dependencies needed
-    ],
     targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "AlonIOS",
-            dependencies: []),
+            name: "AlonIOS"),
         .testTarget(
             name: "AlonIOSTests",
-            dependencies: ["AlonIOS"]),
+            dependencies: ["AlonIOS"]
+        ),
     ]
 )
